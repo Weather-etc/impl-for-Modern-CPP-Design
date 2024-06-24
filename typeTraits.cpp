cb9c96db6d8007ecd90eb7dfea21236d5c774028
faraway <2263704336@qq.com>
2024-06-24 20:41:32 +0800

    updates on section 2.10

--- typeTraits.cpp
+++ typeTraits.cpp
@@ -5,11 +5,30 @@
 struct NullType {};
 struct EmptyType {};
 
+// Int2Type
+template <int v>
+struct Int2Type
+{
+  enum {value = v};
+};
+
+// type select
+template <bool flag, typename T, typename U>
+struct Select
+{
+  typedef T Result;
+};
+template <typename T, typename U>
+{
+  typedef U Result;
+};
+
 template <typename T>
 class TypeTraits 
 {
 private:
   // test pointer type
+  // We use partial template specialization to strip qualifiers or pointer/reference
   template <class U> struct PointerTraits
   {
     enum {result = false};
@@ -45,6 +64,18 @@ private:
     typedef U NonConstType;
   };
 
+  // test volatile qualifier
+  template <class U> struct VolatileTraits
+  {
+    enum {result = false};
+    typedef U NonVolatileType;
+  };
+  template <class U> struct VolatileTraits<volatile U>
+  {
+    enum {result = true};
+    typedef U NonVolatileType;
+  };
+
   // test class member pointer
   template <class U> struct PToMTraits
   {
@@ -60,17 +91,12 @@ public:
   enum {isMemberPointer = PToMTraits::result};
   enum {isConstType = ConstTraits::result};
 
-  typedef typename PointerTraits<T>::PointeeType PointeeType;
-  typedef typename ReferTraits<T>::ReferType ReferencedType;
-  typedef typename ConstTraits<T>::NonConstType NonConstType;
-
   // type traits using typelists, skip details for now
   typedef TYPELIST_4(
       unsigned char, unsigned short int, unsigned int, unsigned long int)
     UnsignedInts;
   typedef TYPELIST_4(
-      signed char, short int, int, long int)
-    SignedInts;
+      signed char, short int, int, long int) SignedInts;
   typedef TYPELIST_3(bool, char, wchar_t) OtherInts;
   typedef TYPELIST_3(float, double, long double) Floats;
 
@@ -80,11 +106,70 @@ public:
   enum {isStdFloat = TL::IndexOf<Floats, T>::value >= 0};
   enum {isStdArith = isStdIntegral || isStdFloat};
   enum {isStdFundamental = isStdArith || Conversion<T, void>::sameType};
+
+  typedef typename PointerTraits<T>::PointeeType PointeeType;
+  typedef typename ReferTraits<T>::ReferType ReferencedType;
+  typedef typename ConstTraits<T>::NonConstType NonConstType;
+  typedef typename Select<isStdArith || isPointerType || isMemberPointer, 
+          T, ReferencedType&>::Result;
 };
+
+
+// A patch for POD(plain old data)
+template <typename T>
+struct SupportBitwiseCopy
+{
+  enum {result = false};
+};
+template <>
+struct SupportBitwiseCopy<POD>
+{
+  enum {result = true};
+};
+
+enum CopyAlgoSelector {Conservative, Fase};
+
+// Conservative routine works for any type. So we use it as primary template
+template <typename InIt, typename OutIt>
+OutIt CopyImpl(InIt first, InIt last, OutIt result, Int2Type<Conservative>)
+{
+  for (; first != last; ++first, ++result)
+    *result = *first;
+  return result;
+}
+// Fast routine works only for pointers to raw data
+template <typename InIt, typename OutIt>
+OutIt CopyImpl (InIt first, InIt last, OutIt result, Int2Type<Fast>)
+{
+  const size_t n = last - first;
+  // another impl for copy
+    return result + n;
+}
+template <typename InIt, typename OutIt>
+OutIt Copy(InIt first, InIt last, OutIt result)
+{
+  typedef typename TypeTraits<InIt>::PointeeType SrcPointee;
+  typedef typename TypeTraits<OutIt>::PointeeType DestPointee;
+  enum {copyAlgo = 
+    TypeTraits<InIt>::isPointer &&
+    TypeTraits<OutIt>::isPointer &&
+    SupportsBitwiseCopy<SrcPointee>::result &&
+    SupportBitwiseCopy<DestPointee>::result &&
+    TypeTraits<SrcPointee>::isStdFundamental &&
+    TypeTraits<DestPointee>::isStdFundamental &&
+    Typetraits<SrcPointee>::isStdFloat == TypeTraits<DestPointee>::isStdFloat &&
+    sizeof(SrcPointee) == sizeof(DestPointee) ? Fast : Conservative};
+  return CopyImpl(first, last, result, Int2Type<copyAlgo>());
+}
 
 
 
 // test code
+struct POD
+{
+  int a;
+};
+
 int main() {
   const bool iterIsPtr = TypeTraits<std::vector<int>::iterator>::isPointer;
   if (iterIsPtr) 
